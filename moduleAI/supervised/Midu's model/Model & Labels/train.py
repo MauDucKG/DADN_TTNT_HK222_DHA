9896977f18728cfdf66f03c1bba9a18296998c58
minhduc <minhduc16102@gmail.com>
2023-05-05 22:55:22 +0700

    Fix path dataset

--- moduleAI/supervised/Midu's model/Model & Labels/train.py
+++ moduleAI/supervised/Midu's model/Model & Labels/train.py
@@ -100,7 +100,7 @@ train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input,rotat
     fill_mode='nearest'   # Fill any empty pixels with the nearest pixel
 )
 
-train_generator = train_datagen.flow_from_directory('./Headshots', target_size=(224,224), color_mode='rgb', batch_size=32, class_mode='categorical', shuffle=True)
+train_generator = train_datagen.flow_from_directory(images_dir, target_size=(224,224), color_mode='rgb', batch_size=32, class_mode='categorical', shuffle=True)
 
 NO_CLASSES = len(train_generator.class_indices.values())
 
